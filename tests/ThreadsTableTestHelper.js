42f4a05bea1b87514c8a07ce262054be6691e886
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-09-11 19:51:10 +0700

    ThreadsTableTestHelper.js

---
+++ tests/ThreadsTableTestHelper.js
@@ -0,0 +1,34 @@
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const UsersTableTestHelper = {
+  async addThread({
+    id = 'thread-123',
+    title = 'title',
+    body = 'body',
+    owner = 'user-123',
+  }) {
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, NOW(), $4)',
+      values: [id, title, body, owner],
+    };
+
+    await pool.query(query);
+  },
+
+  async findThreadById(id) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM threads WHERE 1=1');
+  },
+};
+
+module.exports = UsersTableTestHelper;
