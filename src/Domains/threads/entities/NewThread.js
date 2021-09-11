42f08fa931878109ee345b32998ebade088d7092
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-09-11 15:18:33 +0700

    NewThread entities

---
+++ src/Domains/threads/entities/NewThread.js
@@ -0,0 +1,21 @@
+class NewThread {
+  constructor(payload) {
+    this._verifyPayload(payload);
+  }
+
+  _verifyPayload({ title, body }) {
+    if (!title || !body) {
+      throw new Error('NEW_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof title !== 'string' || typeof body !== 'string') {
+      throw new Error('NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+
+    if (title.length > 100) {
+      throw new Error('NEW_THREAD.TITLE_LIMIT_CHAR');
+    }
+  }
+}
+
+module.exports = NewThread;
---
+++ src/Domains/threads/entities/_test/NewThread.test.js
@@ -0,0 +1,36 @@
+const NewThread = require('../NewThread');
+
+describe('a NewThread entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    const payload = {
+      title: 'judul',
+    };
+
+    expect(() => new NewThread(payload)).toThrowError(
+      'NEW_THREAD.NOT_CONTAIN_NEEDED_PROPERTY'
+    );
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    const payload = {
+      title: 123,
+      body: true,
+    };
+
+    expect(() => new NewThread(payload)).toThrowError(
+      'NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION'
+    );
+  });
+
+  it('should throw error when title contains more than 100 character', () => {
+    const payload = {
+      title:
+        'juduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljuduljudul',
+      body: 'body',
+    };
+
+    expect(() => new NewThread(payload)).toThrowError(
+      'NEW_THREAD.TITLE_LIMIT_CHAR'
+    );
+  });
+});
