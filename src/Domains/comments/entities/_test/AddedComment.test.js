b8eaeb5170b03ff79cb674247175e89e59ceb6d9
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-09-12 16:37:54 +0700

    AddedComment entities

---
+++ src/Domains/comments/entities/_test/AddedComment.test.js
@@ -0,0 +1,40 @@
+const AddedComment = require('../AddedComment');
+
+describe('a AddedComment entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    const payload = {
+      content: 'comment',
+      owner: 'user-123',
+    };
+
+    expect(() => new AddedComment(payload)).toThrowError(
+      'ADDED_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY'
+    );
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    const payload = {
+      id: 'comment-123',
+      content: 123,
+      owner: true,
+    };
+
+    expect(() => new AddedComment(payload)).toThrowError(
+      'ADDED_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION'
+    );
+  });
+
+  it('should create AddedComment object correctly', () => {
+    const payload = {
+      id: 'comment-123',
+      content: 'comment',
+      owner: 'user-123',
+    };
+
+    const { id, content, owner } = new AddedComment(payload);
+
+    expect(id).toEqual(payload.id);
+    expect(content).toEqual(payload.content);
+    expect(owner).toEqual(payload.owner);
+  });
+});
