d34747bde30e914c4adf2ad7f1cd1c64d0c2b01c
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-09-12 20:03:23 +0700

    CommentsTableTestHelper

---
+++ tests/CommentsTableTestHelper.js
@@ -0,0 +1,34 @@
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const CommentsTableTestHelper = {
+  async addComment({
+    id = 'comment-123',
+    content = 'title',
+    owner = 'user-123',
+    threadId = 'thread-123',
+  }) {
+    const query = {
+      text: 'INSERT INTO comments VALUES($1, $2, NOW(), $3, $4)',
+      values: [id, content, owner, threadId],
+    };
+
+    await pool.query(query);
+  },
+
+  async findCommentById(id) {
+    const query = {
+      text: 'SELECT * FROM comments WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM comments WHERE 1=1');
+  },
+};
+
+module.exports = CommentsTableTestHelper;
