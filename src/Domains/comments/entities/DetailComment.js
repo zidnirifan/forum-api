d37a0c2dea92989b74f863d379bc324d42c9d325
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-09-16 19:52:21 +0700

    DetailComment entities

---
+++ src/Domains/comments/entities/DetailComment.js
@@ -0,0 +1,31 @@
+class DetailComment {
+  constructor(payload) {
+    this._verifyPayload(payload);
+
+    const { id, content, username, date, replies } = payload;
+
+    this.id = id;
+    this.content = content;
+    this.username = username;
+    this.date = date;
+    this.replies = replies;
+  }
+
+  _verifyPayload({ id, content, username, date, replies }) {
+    if (!id || !content || !username || !date || !replies) {
+      throw new Error('DETAIL_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (
+      typeof id !== 'string' ||
+      typeof content !== 'string' ||
+      typeof username !== 'string' ||
+      typeof date !== 'string' ||
+      !Array.isArray(replies)
+    ) {
+      throw new Error('DETAIL_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+}
+
+module.exports = DetailComment;
---
+++ src/Domains/comments/entities/_test/DetailComment.test.js
@@ -0,0 +1,48 @@
+const DetailComment = require('../DetailComment');
+
+describe('a DetailComment entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    const payload = {
+      content: 'comment',
+      username: 'user',
+    };
+
+    expect(() => new DetailComment(payload)).toThrowError(
+      'DETAIL_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY'
+    );
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    const payload = {
+      id: 'comment-123',
+      username: true,
+      date: 99,
+      content: 123,
+      replies: {},
+    };
+
+    expect(() => new DetailComment(payload)).toThrowError(
+      'DETAIL_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION'
+    );
+  });
+
+  it('should create DetailComment object correctly', () => {
+    const payload = {
+      id: 'comment-123',
+      username: 'user',
+      date: new Date().toISOString(),
+      content: 'comment',
+      replies: [
+        { id: 'reply-123', content: 'reply', date: '23456', username: 'user' },
+      ],
+    };
+
+    const { id, content, username, date, replies } = new DetailComment(payload);
+
+    expect(id).toEqual(payload.id);
+    expect(username).toEqual(payload.username);
+    expect(date).toEqual(payload.date);
+    expect(content).toEqual(payload.content);
+    expect(replies).toStrictEqual(payload.replies);
+  });
+});
