716e2d6d543ff7d11edf37284f5346aae14b842f
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-09-18 19:47:53 +0700

    add replyRepository to GetDetailThreadUseCase in container

--- src/Infrastructures/container.js
+++ src/Infrastructures/container.js
@@ -266,6 +266,10 @@ container.register([
           name: 'threadRepository',
           internal: ThreadRepository.name,
         },
+        {
+          name: 'replyRepository',
+          internal: ReplyRepository.name,
+        },
       ],
     },
   },
