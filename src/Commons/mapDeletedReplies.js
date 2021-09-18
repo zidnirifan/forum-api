22c2d3cd63c9ec313a6b2e50d482d3e752bbb179
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-09-18 20:44:55 +0700

    change name parameter

--- src/Commons/mapDeletedReplies.js
+++ src/Commons/mapDeletedReplies.js
@@ -1,5 +1,5 @@
-const mapDeletedReplies = (comments) =>
-  comments.map(({ is_delete, content, ...rest }) =>
+const mapDeletedReplies = (replies) =>
+  replies.map(({ is_delete, content, ...rest }) =>
     is_delete
       ? { content: '**balasan telah dihapus**', ...rest }
       : { content, ...rest }
