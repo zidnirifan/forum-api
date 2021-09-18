a808731259696972f4bb08952e319950e6a02270
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-09-18 11:00:09 +0700

    add isReplyExist, deleteReply, verifyReplyOwner function on ReplyRepository

--- src/Domains/replies/ReplyRepository.js
+++ src/Domains/replies/ReplyRepository.js
@@ -2,6 +2,18 @@ class ReplyRepository {
   async addReply(newReply) {
     throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async isReplyExist(replyId) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteReply(replyId) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyReplyOwner({ id, owner }) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = ReplyRepository;
--- src/Domains/replies/_test/ReplyRepository.test.js
+++ src/Domains/replies/_test/ReplyRepository.test.js
@@ -7,5 +7,14 @@ describe('ReplyRepository interface', () => {
     await expect(replyRepository.addReply({})).rejects.toThrowError(
       'REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'
     );
+    await expect(replyRepository.isReplyExist('')).rejects.toThrowError(
+      'REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'
+    );
+    await expect(replyRepository.verifyReplyOwner('')).rejects.toThrowError(
+      'REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'
+    );
+    await expect(replyRepository.deleteReply('')).rejects.toThrowError(
+      'REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'
+    );
   });
 });
