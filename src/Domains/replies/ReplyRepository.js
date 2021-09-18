6f5f7d591d32784386e4e0f85a2259c29c2079cc
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-09-18 09:13:53 +0700

    ReplyRepository

---
+++ src/Domains/replies/ReplyRepository.js
@@ -0,0 +1,7 @@
+class ReplyRepository {
+  async addReply(newReply) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ReplyRepository;
---
+++ src/Domains/replies/_test/ReplyRepository.test.js
@@ -0,0 +1,11 @@
+const ReplyRepository = require('../ReplyRepository');
+
+describe('ReplyRepository interface', () => {
+  it('should throw error when invoke abstract behavior', async () => {
+    const replyRepository = new ReplyRepository();
+
+    await expect(replyRepository.addReply({})).rejects.toThrowError(
+      'REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'
+    );
+  });
+});
