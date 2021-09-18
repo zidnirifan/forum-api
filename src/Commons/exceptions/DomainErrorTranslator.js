7d938241d242b28474fd84a6ed75ea660c7d2218
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-09-18 10:38:13 +0700

    translate domain error reply

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -48,6 +48,12 @@ DomainErrorTranslator._directories = {
   'NEW_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError(
     'tidak dapat menambahkan comment baru karena tipe data tidak sesuai'
   ),
+  'NEW_REPLY.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError(
+    'tidak dapat menambahkan balasan baru karena properti yang dibutuhkan tidak ada'
+  ),
+  'NEW_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError(
+    'tidak dapat menambahkan balasan baru karena tipe data tidak sesuai'
+  ),
 };
 
 module.exports = DomainErrorTranslator;
