ada75979def6322bf19093c38c07d13004cbc87a
Zidni Rif'an <zidni.rifan7@gmail.com>
2021-09-27 15:23:14 +0700

    add findLikeCommentById and cleanTable function

--- tests/UserCommentLikesTableTestHelper.js
+++ tests/UserCommentLikesTableTestHelper.js
@@ -14,6 +14,20 @@ const UsersCommentLikesTableTestHelper = {
 
     await pool.query(query);
   },
+
+  async findLikeCommentById(id) {
+    const query = {
+      text: 'SELECT * FROM user_comment_likes WHERE id = $1',
+      values: [id],
+    };
+
+    const { rows } = await pool.query(query);
+    return rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM user_comment_likes WHERE 1=1');
+  },
 };
 
 module.exports = UsersCommentLikesTableTestHelper;
